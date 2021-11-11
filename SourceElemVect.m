86ee90f1b3d8b754cd809b88b422bd81a1f1f090
dhamiles <93228218+dhamiles@users.noreply.github.com>
2021-11-11 23:38:16 +0000

    Fixed Typo

--- SourceElemVect.m
+++ SourceElemVect.m
@@ -1,5 +1,5 @@
 function elevec = SourceElemVect(f,eID,msh)
-%REACTIONELEMMATRIX Returns 2x1 local element column vector  
+%SOURCEELEMVECT Returns 2x1 local element column vector  
 %   Function returns the 2x1 local element vector for a given mesh (msh), 
 %   for the source term (f) at a local element (eID)
 
