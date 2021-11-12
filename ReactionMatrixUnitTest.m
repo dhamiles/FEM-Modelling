15a9d07510e27102a70630866fc3b40fb7a6a366
Dominic Macbook <dommiles-ha@hotmail.com>
2021-11-12 12:56:05 +0000

    Release v1.2
    Bugfixes and adding more unit testing code

--- ReactionMatrixUnitTest.m
+++ ReactionMatrixUnitTest.m
@@ -1,7 +1,7 @@
 %% Test 1: test symmetry of the matrix
 % % Test that this matrix is symmetric
 tol = 1e-14;
-lambda = -5; %diffusion coefficient
+lambda = -5; %reaction coefficient
 eID=1; %element ID
 msh = OneDimLinearMeshGen(0,1,10);
 
@@ -12,7 +12,7 @@ assert(abs(elemat(1,2) - elemat(2,1)) <= tol)
 % % Test that for two elements of an equispaced mesh, as described in the
 % % lectures, the element matrices calculated are the same
 tol = 1e-14;
-lambda = 7; %diffusion coefficient
+lambda = 7; %reaction coefficient
 eID=1; %element ID
 msh = OneDimLinearMeshGen(0,1,10);
 
@@ -30,7 +30,7 @@ assert(abs(diffnorm) <= tol)
 % % Test that element 1's matrix from the example in the lectures (3
 % % elements)evaluates correctly
 tol = 1e-14;
-lambda = 3; %diffusion coefficient
+lambda = 3; %reaction coefficient
 eID=1; %element ID
 msh = OneDimLinearMeshGen(0,1,3);
 
@@ -41,3 +41,17 @@ diff = elemat1 - elemat2; %calculate the difference between the two matrices
 diffnorm = sum(sum(diff.*diff)); %calculates the total squared error between the matrices
 assert(abs(diffnorm) <= tol)
 
+%% Test 4: test an element in non equi-spaced mesh is correct
+% % Evaluates that the last element in a non equi-spaced mesh is evaluated
+% % correctly
+tol = 1e-14;
+lambda = -9; %reaction coefficient
+eID=4; %element ID
+msh = OneDimSimpleRefinedMeshGen(0,1,4);
+
+elemat1 = ReactionElemMatrix(lambda,eID,msh);
+
+elemat2 = [ -0.3750 -0.1875; -0.1875 -0.3750];
+diff = elemat1 - elemat2; %calculate the difference between the two matrices
+diffnorm = sum(sum(diff.*diff)); %calculates the total squared error between the matrices
+assert(abs(diffnorm) <= tol)
--- SourceElemVect.m
+++ SourceElemVect.m
@@ -2,6 +2,7 @@ function elevec = SourceElemVect(f,eID,msh)
 %SOURCEELEMVECT Returns 2x1 local element column vector  
 %   Function returns the 2x1 local element vector for a given mesh (msh), 
 %   for the source term (f) at a local element (eID)
+%   NOTE: this current only functions for a CONSTANT value for f!
 
 % Pull the value of J out of the mesh data structure at element eID
 J = msh.elem(eID).J;
